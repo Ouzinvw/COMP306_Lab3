16a7f316afa111c889148621b4a3d616959f5b1f
Ouzinvw <ouzinvw@live.com>
2023-11-01 11:09:54 -0400

    Add project files.

---
+++ 301229915(Jeffers&vonWahl)_Lab3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _301229915_Jeffers_vonWahl__Lab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
