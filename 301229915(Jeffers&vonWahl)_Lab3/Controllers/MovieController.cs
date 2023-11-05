0f092755d9ecef83a1ce6ae642c809906a9510ca
Ouzinvw <ouzinvw@live.com>
2023-11-05 10:23:20 -0500

    movie CRUD completed

--- 301229915(Jeffers&vonWahl)_Lab3/Controllers/MovieController.cs
+++ 301229915(Jeffers&vonWahl)_Lab3/Controllers/MovieController.cs
@@ -28,7 +28,58 @@ namespace HuluWeb.Controllers
             obj.uploaderId = 1; // TEMPORARY - Add uploaderId retrieval
             _db.Movies.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Movie created successfully!";
 			return RedirectToAction("Index");
+		}
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Movie movieFromDb = _db.Movies.Find(id);
+            if (movieFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(movieFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie obj)
+        {
+            _db.Movies.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Movie updated successfully!";
+            return RedirectToAction("Index");
+        }
+
+		public IActionResult Delete(int? id) 
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Movie movieFromDb = _db.Movies.Find(id);
+			if (movieFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(movieFromDb);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+            Movie? obj = _db.Movies.Find(id); if (obj == null)
+            {
+                return NotFound();
+            }
+			_db.Movies.Remove(obj);
+			_db.SaveChanges();
+            TempData["success"] = "Movie deleted successfully!";
+            return RedirectToAction("Index");
 		}
 	}
 }
---
+++ 301229915(Jeffers&vonWahl)_Lab3/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using HuluWeb.Data;
+using HuluWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HuluWeb.Controllers
+{
+	public class UserController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public UserController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+	}
+}
